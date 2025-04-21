4b647e62a785c33b4b17ddfd0df9e223dd7e4cab
Vijay Pandian <ananth.vijay@gmail.com>
2025-04-21 11:09:17 -0400

    Update setup-docker.sh - use sudo for commands

--- setup-docker.sh
+++ setup-docker.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 set -e
 
+export DEBIAN_FRONTEND=noninteractive
+UNAME="$(uname -r)"
+export UNAME
+
 echo "-------------------------------------------"
 echo "     Performing System Updates"
 echo "-------------------------------------------"
-apt-get update && apt-get -y upgrade
+sudo apt-get update && apt-get -y upgrade
 
 echo "--------------------------------------"
 echo "        Installing NTP and Git"
 echo "--------------------------------------"
-apt-get install -y ntp git
+sudo apt-get install -y ntp git
 
 echo "--------------------------------------"
 echo "        Installing Docker"
 echo "--------------------------------------"
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get install -y "linux-image-$UNAME"
-apt-get update
-apt-get -y install docker-ce=5:25.0.2-1~ubuntu.20.04~focal \
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get install -y "linux-image-$UNAME"
+sudo apt-get update
+sudo apt-get -y install docker-ce=5:25.0.2-1~ubuntu.20.04~focal \
                    docker-ce-cli=5:25.0.2-1~ubuntu.20.04~focal
 
 echo "Adding ec2-user to docker group..."
